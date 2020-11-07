706ccd77cb954c7c62e131192dd65de917e5c661
Jhony Vidal <jhony.oliveira@mulesoft.com>
2020-11-07 18:18:53 -0300

    Add newDeck() fn

--- deck.go
+++ deck.go
@@ -6,6 +6,21 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Heards", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 // Receiver: Any variable of type "deck" gets access to the methods define on the type
 func (d deck) print() {
 	for i, card := range d {
--- main.go
+++ main.go
@@ -2,10 +2,7 @@ package main
 
 // Main will be used to create and manipulate a deck of cards
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	// append() creates a new slice and returns a new one
-	cards = append(cards, "Six of Spades")
-
+	cards := newDeck()
 	cards.print()
 }
 
